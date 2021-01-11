72f3bc2d6ba9874cd66bd4f705c102cdcee05b7a
jenny.cipolli@docomodigital.com <jenny.cipolli@docomodigital.com>
2021-01-11 10:43:23 +0100

    add redux and user reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./header.style.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 
 
 
@@ -26,4 +27,10 @@ const Header = ({ currentUser }) => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = state => ({
+
+    currentUser: state.user.currentUser
+
+})
+
+export default connect(mapStateToProps)(Header);
