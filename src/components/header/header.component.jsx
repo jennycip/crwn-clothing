94726a724beb14f5fbda3cef047cce5b28b9eab7
jenny.cipolli@docomodigital.com <jenny.cipolli@docomodigital.com>
2021-01-11 12:24:09 +0100

    icon shopping cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,6 +4,7 @@ import "./header.style.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+import CartIcon from '../cart-icon/cart-icon-component';
 
 
 
@@ -22,6 +23,8 @@ const Header = ({ currentUser }) => (
                     :
                     <Link className='option' to='/signin'>SIGN IN</Link>
             }
+
+            <CartIcon />
 
         </div>
     </div>
